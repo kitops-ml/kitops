cecd0f9328017dfc9c5a0c5794db437780b7d81d
Angel Misevski <amisevsk@gmail.com>
2025-05-20 16:44:53 -0400

    Update kit init container entrypoint to support extra flags
    Add support for '$EXTRA_FLAGS' environment variable in the Kit init
    container to allow specifying arbitrary flags for the unpack command. If
    this environment variable is used to specify the --dir or --filter
    flags, exit with an error.
    
    By default, use '--ignore-existing' when unpacking to ensure we can run
    container with mounted persistent storage without erroring (if files
    e.g. exist). This behavior can be disabled by setting an alternate value
    for '$EXTRA_FLAGS'
    
    Signed-off-by: Angel Misevski <amisevsk@gmail.com>

--- build/dockerfiles/init/entrypoint.sh
+++ build/dockerfiles/init/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 # Kit configuration via env vars:
 #   - UNPACK_PATH   : Where to unpack the modelkit (default: /home/user/modelkit)
 #   - UNPACK_FILTER : What to unpack from the modelkit (default: model)
+#   - EXTRA_FLAGS   : Additional flags for the unpack operation (default: --ignore-existing)
 #   - MODELKIT_REF  : The modelkit to unpack -- required!
 UNPACK_PATH=${UNPACK_PATH:-/home/user/modelkit/}
 UNPACK_FILTER=${UNPACK_FILTER:-model}
@@ -12,6 +13,19 @@ if [ -z "$MODELKIT_REF" ]; then
   echo "Environment variable \$MODELKIT_REF is required"
   exit 1
 fi
+
+read -r -a ADDITIONAL_UNPACK_FLAGS <<< "$EXTRA_FLAGS"
+for flag in "${ADDITIONAL_UNPACK_FLAGS[@]}"; do
+  if [ "$flag" == "--filter" ] || [ "$flag" == "-f" ] || [ "$flag" == "--dir" ] || [ "$flag" == "-d" ]; then
+    echo "Do not use \$EXTRA_FLAGS to specify '--filter' and '--dir' flags"
+    echo "Set environment variables \$UNPACK_PATH and \$UNPACK_FILTER instead"
+    exit 1
+  fi
+done
+if [ ${#ADDITIONAL_UNPACK_FLAGS[@]} == "0" ]; then
+  ADDITIONAL_UNPACK_FLAGS[0]="--ignore-existing"
+fi
+
 
 # Variables for verifying signature via cosign. Can specify either a key to use for
 # verifying or an identity and oidc issuer for keyless verification
@@ -31,5 +45,5 @@ fi
 echo "Binary version info:"
 kit version
 
-echo "Unpacking modelkit $MODELKIT_REF to $UNPACK_PATH with filter $UNPACK_FILTER"
-kit unpack "$MODELKIT_REF" --dir "$UNPACK_PATH" --filter="$UNPACK_FILTER"
+echo "Unpacking modelkit $MODELKIT_REF to $UNPACK_PATH with filter '$UNPACK_FILTER'. Additional flags: ${ADDITIONAL_UNPACK_FLAGS[@]}"
+kit unpack "$MODELKIT_REF" --dir "$UNPACK_PATH" --filter="$UNPACK_FILTER" "${ADDITIONAL_UNPACK_FLAGS[@]}"
