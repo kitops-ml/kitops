eb7621705d5fd816848bed04051bb996b9947b77
Omar Bassem <omarbassem889@gmail.com>
2026-03-06 16:28:21 +0200

    refactor: remove closures from run commands
    Returning a closure from a function is unnecessary in this case, as there are no dependencies being injected to the wrapper function.
    
    Make functions private for now as they aren't being called outside the package.
    
    Issue: kitops-ml#419
    
    Signed-off-by: Omar Bassem <omarbassem889@gmail.com>

--- pkg/cmd/config/cmd.go
+++ pkg/cmd/config/cmd.go
@@ -11,48 +11,45 @@ func ConfigCommand() *cobra.Command {
 		Short: configShortDesc,
 		Long:  configLongDesc,
 	}
-	cmd.AddCommand(ConfigSetCommand())
-	cmd.AddCommand(ConfigGetCommand())
+	cmd.AddCommand(configSetCommand())
+	cmd.AddCommand(configGetCommand())
 
 	return cmd
 }
 
-func ConfigSetCommand() *cobra.Command {
+func configSetCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "set KEY VALUE",
 		Short: setConfigShortDesc,
 		Long:  setConfigLongDesc,
-		RunE:  runSetCommand(),
+		RunE:  runSetCommand,
 	}
 	cmd.Args = cobra.ExactArgs(2)
 
 	return cmd
 }
 
-func runSetCommand() func(cmd *cobra.Command, args []string) error {
-	return func(cmd *cobra.Command, args []string) error {
+func runSetCommand(cmd *cobra.Command, args []string) error {
 		err := setConfig(args[0], args[1])
 		if err != nil {
 			return output.Fatalf("%s", err)
 		}
 		return nil
 	}
-}
 
-func ConfigGetCommand() *cobra.Command {
+func configGetCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "get KEY",
 		Short: getConfigShortDesc,
 		Long:  getConfigLongDesc,
-		RunE:  runGetCommand(),
+		RunE:  runGetCommand,
 	}
 	cmd.Args = cobra.ExactArgs(1)
 
 	return cmd
 }
 
-func runGetCommand() func(cmd *cobra.Command, args []string) error {
-	return func(cmd *cobra.Command, args []string) error {
+func runGetCommand(cmd *cobra.Command, args []string) error {
 		val, err := getConfig(args[0])
 		if err != nil {
 			return output.Fatalf("%s", err)
@@ -60,4 +57,3 @@ func runGetCommand() func(cmd *cobra.Command, args []string) error {
 		output.Infoln(val)
 		return nil
 	}
-}
