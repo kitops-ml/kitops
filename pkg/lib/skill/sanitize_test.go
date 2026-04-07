25c3382f5057554c2b500469fa1dbaf42d406c9f
Gorkem Ercan <gorkem.ercan@gmail.com>
2026-04-07 12:44:18 -0400

    Address PR review: homeDir error handling, stale comment, copyright year
    - Fail fast in GetGlobalSkillsDir and DetectInstalledAgents when
      user home directory cannot be determined
    - Fix stale comment referencing path.Rel in stripPromptPrefix
    - Align copyright year to 2026 in sanitize_test.go
    
    Signed-off-by: Gorkem Ercan <gorkem.ercan@gmail.com>

--- pkg/lib/skill/sanitize_test.go
+++ pkg/lib/skill/sanitize_test.go
@@ -1,4 +1,4 @@
-// Copyright 2024 The KitOps Authors.
+// Copyright 2026 The KitOps Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
